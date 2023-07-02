ecbb8828a534ed0d4d41b2709f38e272f6c1e4d9
Max Halford <maxhalford25@gmail.com>
2023-07-03 00:38:18 +0200

    add summary

---
+++ scripts/update_summary.py
@@ -0,0 +1,35 @@
+from cities import cities
+
+def main():
+    with open('README.md', 'r') as f:
+        readme_contents = f.readlines()
+
+    # Find the start and end indices of the table you want to replace
+    table_start = None
+    table_end = None
+    for i, line in enumerate(readme_contents):
+        if line.startswith('| Country'):
+            table_start = i
+        elif ".geojson" in line and table_start is not None:
+            table_end = max(i, table_end or 0)
+
+    # Make sure the table was found
+    if table_start is None or table_end is None:
+        raise RuntimeError('Table not found in README.md')
+
+    # Generate the new table content
+    new_table = []
+    new_table.append('| Country | Name | Live |\n')
+    new_table.append('|---------|------|------|\n')
+    for city in sorted(cities, key=lambda c: c.country + c.name):
+        new_table.append(f"| {city.country} | {city.name} | [`{city.name}.geojson`](data/stations/{city.name}.geojson) |\n")
+
+    # Replace the existing table with the new table
+    readme_contents[table_start:table_end+1] = new_table
+
+    # Write the modified contents back to the README.md file
+    with open('README.md', 'w') as f:
+        f.writelines(readme_contents)
+
+if __name__ == "__main__":
+    main()
